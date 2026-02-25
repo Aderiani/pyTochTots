e55a4dfd99213fe1c034e50788f8466a2b29fb79
Aderiani <a.rezaeiadriani@gmail.com>
2026-02-25 11:38:26 +0100

    modied3

--- fundementals.py
+++ fundementals.py
@@ -13,6 +13,7 @@ image_path = data_path / "pizza_steak_sushi"
 
 
 # If the image folder doesn't exist, download it and prepare it... 
+
 if image_path.is_dir():
     print(f"{image_path} directory exists.")
 else:
